2c8fe202c8c804d8bae88e15b1eb381c6ab6b066
Mika Rinne <mika.rinne@oracle.com>
2017-08-02 15:10:01 +0300

    added kubernetes

--- backends/client.go
+++ backends/client.go
@@ -11,6 +11,7 @@ import (
 	"github.com/mikarinneoracle/confd/backends/rancher"
 	"github.com/mikarinneoracle/confd/backends/redis"
 	"github.com/mikarinneoracle/confd/backends/stackengine"
+    "github.com/mikarinneoracle/confd/backends/kubernetes"
 	"github.com/mikarinneoracle/confd/backends/vault"
 	"github.com/mikarinneoracle/confd/backends/zookeeper"
 	"github.com/mikarinneoracle/confd/log"
@@ -65,6 +66,8 @@ func New(config Config) (StoreClient, error) {
 		return dynamodb.NewDynamoDBClient(table)
 	case "stackengine":
 		return stackengine.NewStackEngineClient(backendNodes, config.Scheme, config.ClientCert, config.ClientKey, config.ClientCaKeys, config.AuthToken)
+    case "kubernetes":
+		return kubernetes.NewKubernetesClient(backendNodes, config.Scheme, config.ClientCert, config.ClientKey, config.ClientCaKeys, config.AuthToken)
 	}
 	return nil, errors.New("Invalid backend")
 }
